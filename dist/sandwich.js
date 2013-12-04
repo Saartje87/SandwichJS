ad7dfdc5cb5ef06a89a3a81b467a8d5d6dc0a44b
Niek Saarberg <n.saarberg@gmail.com>
2013-12-04 23:48:30 +0100

    Dist updated

--- dist/sandwich.js
+++ dist/sandwich.js
@@ -5,7 +5,7 @@
  * Copyright 2013 Niek Saarberg
  * Licensed MIT
  *
- * Build date 2013-12-04 20:25
+ * Build date 2013-12-04 23:46
  */
 (function ( name, context, definition ) {
 	
@@ -84,7 +84,14 @@ Sandwich.Module = {
 			dependencies.push(Sandwich.Module.start(moduleName));
 		});
 
-		return module.instance = module.module.apply(null, dependencies);
+		module.instance = module.module.apply(null, dependencies);
+
+		if( typeof module.instance.onStart === 'function' ) {
+
+			module.instance.onStart();
+		}
+
+		return module.instance;
 	},
 
 	/**
@@ -95,9 +102,6 @@ Sandwich.Module = {
 		return _Modules;
 	}
 };
-
-// Sandwich.Module.start('Router');
-
 
 var _AppInit = false;
 
@@ -146,16 +150,6 @@ Sandwich.Application = {
 	}
 };
 Sandwich.Module.define('Router', ['Route'], function ( Route ) {
-
-	if( !('onhashchange' in window) ) {
-
-		throw new Error('Browser not supported');
-	}
-
-	PB.$(window).on('hashchange', execRoute);
-
-	// Execute first route when dom is ready (hopefully this will always trigger after all js is loaded)
-	PB.ready(execRoute);
 
 	/**
 	 *
@@ -221,6 +215,19 @@ Sandwich.Module.define('Router', ['Route'], function ( Route ) {
 	}
 
 	return {
+
+		onStart: function () {
+
+			if( !('onhashchange' in window) ) {
+
+				throw new Error('Browser not supported');
+			}
+
+			PB.$(window).on('hashchange', execRoute);
+
+			// Execute first route when dom is ready (hopefully this will always trigger after all js is loaded)
+			PB.ready(execRoute);
+		},
 
 		/**
 		 *
@@ -358,6 +365,210 @@ Sandwich.Module.define('Route', function () {
 		}
 	};
 });
+var _Models = {};
+
+// Declare Sync object
+Sandwich.Sync = {};
+
+var Model = function () {
+
+	this.attributes = {};
+
+	// Client-id
+	this.cid = guid();
+};
+
+Model.create = function ( modelName, config ) {
+
+	if( _Models[modelName] ) {
+
+		Sandwich.Error.report('Model `'+modelName+'` already declared');
+	}
+
+	// Tmp hack
+	Model.prototype.name = modelName;
+
+	_Models[modelName] = Model;
+	// _Models[modelName] = PB.Class(Model, PB.extend({model: modelName}, config));
+};
+
+Model.factory = function ( modelName ) {
+
+	if( !_Models[modelName] ) {
+
+		Model.create(modelName);
+	}
+
+	return new _Models[modelName]();
+};
+
+Model.prototype = {
+
+	name: null,
+
+	idAttribute: 'id',
+
+	sync: 'RESTful',
+
+	/**
+	 *
+	 */
+	/*find: function () {
+
+		return Collection.factory(this.model).find();
+	},*/
+
+	/**
+	 * 
+	 */
+	findOne: function ( id ) {
+
+		var sync = Sandwich.Sync[this.sync];
+
+		if( !sync ) {
+
+			Sandwich.Error.report('No valid sync given, '+this.sync);
+		}
+
+		// Should be so we return a new object, read should also check if model already exsting in memory
+		// Model.factory(this.name).read(id);
+
+		this.set('id', id);
+
+		sync('read', this);
+	},
+
+	get: function ( key ) {
+
+		return this.attributes[key];
+	},
+
+	has: function ( key ) {
+
+		return this.get(key) !== undefined;
+	},
+
+	set: function ( key, value ) {
+
+		if( value === this.get(key) ) {
+
+			return this;
+		}
+
+		this.attributes[key] = value;
+
+		return this;
+	},
+
+	setData: function ( data ) {
+
+		var key;
+
+		for( key in data ) {
+
+			if( data.hasOwnProperty(key) ) {
+
+				this.set(key, data[key]);
+			}
+		}
+
+		return this;
+	},
+
+	unset: function ( key ) {
+
+		delete this.attributes[key];
+	},
+
+	clear: function () {},
+
+	fetch: function () {},
+
+	save: function () {},
+
+	destroy: function () {},
+
+	url: function () {
+
+		if( this.isNew() ) {
+
+			return '/'+this.name+'/rest/';
+		}
+
+		return '/'+this.name+'/rest/'+this.get(this.idAttribute);
+	},
+
+	// Parse response
+	parse: function ( data ) {
+
+		this.set(data);
+	},
+
+	clone: function () {},
+
+	isNew: function () {
+
+		return !this.has(this.idAttribute);
+	},
+
+	isValid: function () {}
+};
+
+Sandwich.Application.register('Model', function () {
+
+	return Model;
+});
+
+// Tmp location
+function s4 () {
+
+	return Math.floor((1 + Math.random()) * 0x10000)
+		.toString(16)
+		.substring(1);
+};
+
+function guid () {
+
+	return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+};
+// Map from CRUD to HTTP for our default `Backbone.sync` implementation.
+var methodMap = {
+	'create': 'POST',
+	'update': 'PUT',
+	//'patch': 'PATCH',
+	'delete': 'DELETE',
+	'read': 'GET'
+};
+
+Sandwich.Sync.RESTful = function ( method, model, options ) {
+
+	var callback,
+		params = {
+
+			url: model.url(),
+			method: methodMap[method]
+		};
+
+	if( params.method === 'GET' ) {
+
+		// We should call model.parse with response
+		callback = model.parse.bind(model);
+	} else {
+
+		callback = function () {};
+	}
+
+	new PB.Request(params).on('end', function ( t, code ) {
+
+		if( !t || !t.responseJSON ) {
+
+			Sandwich.Error.report('Unexpected JSON response', t.responseText);
+			return;
+		}
+
+		callback(t.responseJSON);
+	}).send();
+};
 return Sandwich;
 });
 
