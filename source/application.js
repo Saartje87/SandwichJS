dbbc0bd802a64e2668e0bba4a8b7b8520480bb7d
Niek Saarberg <n.saarberg@gmail.com>
2013-12-06 09:42:34 +0100

    Added options argument to Sandwich.Application.create(). Updated Sandwich.Application

--- source/application.js
+++ source/application.js
@@ -2,27 +2,35 @@ var _AppInit = false;
 
 Sandwich.Application = {
 
+	// From where your app operates
 	rootElement: 'body',
 
+	// List of registered modules
 	_modules: {},
 
 	/**
 	 * Create out application!
+	 *
+	 * @param {Object} *optional options
+	 * @return {Object} Application namespace
+	 * @throws {Error} when application has already been created
 	 */
-	create: function () {
+	create: function ( options ) {
 
 		var key,
-			app = Sandwich.Application,
-			modules = app._modules,
+			modules = this._modules,
 			App = {};
+
+		options || (options = {});
 
 		if( _AppInit ) {
 
 			return Sandwich.Error.report('Sandwich.Application.create already initialized');
 		}
 
-		App.rootElement = PB.$(app.rootElement);
+		App.rootElement = PB.$(options.rootElement || this.rootElement);
 
+		// Initialize modules
 		for( key in modules ) {
 
 			if( modules.hasOwnProperty(key) ) {
@@ -37,7 +45,12 @@ Sandwich.Application = {
 	},
 
 	/**
+	 * Register a startup function.
 	 *
+	 * Regsitered functions will be triggered once when `Sandwich.Application.create()` is called.
+	 *
+	 * @param {String} Module name
+	 * @param {Function} Module initializer
 	 */
 	register: function ( moduleName, module ) {
 
